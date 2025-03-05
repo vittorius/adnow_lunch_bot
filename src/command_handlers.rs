ff80f6b3191e5276ce13d89ac61747050c973d32
Viktor Zahorodnii <viktor.zahorodnii@railsware.com>
2025-03-05 17:42:12 +0200

    WIP

--- src/command_handlers.rs
+++ src/command_handlers.rs
@@ -50,13 +50,14 @@ pub(crate) async fn go_cmd(bot: &Bot, msg: &Message, bot_service: &BotService) -
     };
 
     // TODO: extract this into a function (see the `BotExt` trait)
-    if let Err(error) = bot.stop_poll(msg.chat.id, MessageId(poll.poll_msg_id)).await {
-        match error {
-            // we swallow the Telegram API error for a case when there is a stored poll but no Telegram poll
-            RequestError::Api(_) => {}
-            _ => return Err(anyhow::Error::new(error)),
-        }
-    }
+    // if let Err(error) = bot.stop_poll(msg.chat.id, MessageId(poll.poll_msg_id)).await {
+    //     match error {
+    //         // we swallow the Telegram API error for a case when there is a stored poll but no Telegram poll
+    //         RequestError::Api(_) => {}
+    //         _ => return Err(anyhow::Error::new(error)), // TODO: such a wrapping is probably wrong, fix it
+    //     }
+    // }
+    bot.stop_poll(msg.chat.id, MessageId(poll.poll_msg_id)).await?;
 
     let voters = &mut *poll.yes_voters;
     if voters.is_empty() {
@@ -108,10 +109,12 @@ mod tests {
     use crate::{build_update_handler, BotService};
     use delegate::delegate;
     use sqlx::PgPool;
-    use teloxide::dptree;
-    use teloxide::types::{ChatId, MessageId};
+    use teloxide::dispatching::{UpdateFilterExt, UpdateHandler};
+    use teloxide::{dptree, Bot};
+    use teloxide::prelude::Requester;
+    use teloxide::types::{ChatId, MessageId, Poll, Update};
     use teloxide::utils::command::BotCommands;
-    use teloxide_tests::{IntoUpdate, MockBot, MockMessageText, Responses};
+    use teloxide_tests::{IntoUpdate, MockBot, MockMessagePoll, MockMessageText, Responses};
 
     struct Environment {
         bot: MockBot,
@@ -121,8 +124,8 @@ mod tests {
 
     impl Environment {
         fn new<TUpdate: IntoUpdate>(db_pool: PgPool, update: TUpdate) -> Self {
-            let bot_service = BotService::new("".to_string(), db_pool);
-            let bot = MockBot::new(update, build_update_handler());
+            let bot_service = BotService::new("1234567890".to_string(), db_pool);
+            let bot = MockBot::new(update, Self::build_test_update_handler());
             bot.dependencies(dptree::deps![bot_service.clone()]);
 
             let chat_id = bot.updates.lock().unwrap().last().unwrap().chat().map(|chat| chat.id);
@@ -140,19 +143,41 @@ mod tests {
 
                 #[call(get_responses)]
                 pub fn bot_responses(&self) -> Responses;
+
+                #[call(update)]
+                pub fn update<T: IntoUpdate>(&self, update: T);
             }
         }
 
-        async fn create_incomplete_poll(&self) {
+        async fn create_poll_in_db(&self, poll_id: Option<&str>, poll_msg_id: Option<MessageId>) {
             self.bot_service
                 .create_poll(
-                    "",
-                    MessageId(0),
+                    poll_id.unwrap_or(MockMessagePoll::POLL_ID),
+                    poll_msg_id.unwrap_or(MessageId(MockMessageText::new().id.0)),
                     self.chat_id
                         .expect("Previous update did not have a chat associated with it"),
                 )
                 .await
                 .expect("Failed to create a fixture poll");
+        }
+
+        // async fn create_and_start_poll(&self) {
+        //     self.bot
+        //         .bot
+        //         .send_poll(
+        //             self.chat_id
+        //                 .expect("Previous update did not have a chat associated with it"),
+        //             "Dummy poll",
+        //             vec!["Yes".into(), "No".into()],
+        //         )
+        //         .await
+        //         .expect("Failed to send a fixture poll");
+        //     self.create_poll().await;
+        // }
+        fn build_test_update_handler() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>> {
+            build_update_handler().branch(
+                Update::filter_poll().endpoint(|_bot_service: BotService, _bot: Bot, _poll: Poll| async { Ok(()) }),
+            )
         }
     }
 
@@ -172,7 +197,7 @@ mod tests {
     async fn on_incomplete_poll_lunch_cmd_sends_notice_and_exits(db_pool: PgPool) {
         let message = MockMessageText::new().text("/lunch");
         let env = Environment::new(db_pool, message);
-        env.create_incomplete_poll().await;
+        env.create_poll_in_db(None, None).await;
         env.bot_dispatch().await;
 
         let responses = env.bot_responses();
@@ -187,7 +212,11 @@ mod tests {
         let env = Environment::new(db_pool, message);
         env.bot_dispatch().await;
 
-        let poll = env.bot_service.get_poll_by_chat_id(env.chat_id.unwrap()).await.expect("Failed to get poll");
+        let poll = env
+            .bot_service
+            .get_poll_by_chat_id(env.chat_id.unwrap())
+            .await
+            .expect("Failed to get poll");
         assert!(poll.is_some());
     }
 
@@ -197,7 +226,56 @@ mod tests {
         let env = Environment::new(db_pool, message);
         env.bot_dispatch().await;
 
-        let poll = env.bot_service.get_poll_by_chat_id(env.chat_id.unwrap()).await.expect("Failed to get poll");
+        let poll = env
+            .bot_service
+            .get_poll_by_chat_id(env.chat_id.unwrap())
+            .await
+            .expect("Failed to get poll");
         assert!(poll.is_some());
     }
+
+    #[sqlx::test]
+    async fn on_non_existent_poll_go_cmd_sends_notice_and_exits(db_pool: PgPool) {
+        let message = MockMessageText::new().text("/go");
+        let env = Environment::new(db_pool, message);
+        env.bot_dispatch().await;
+
+        let responses = env.bot_responses();
+        assert_eq!(responses.sent_messages.len(), 1);
+        let message = responses.sent_messages.last().expect("No sent messages were detected!");
+        assert_eq!(
+            message.text(),
+            Some("Створіть нове опитування, використовуючи команду /lunch.")
+        );
+    }
+
+    #[sqlx::test]
+    async fn on_existing_poll_go_cmd_stops_the_poll(db_pool: PgPool) {
+        // FIXME: send dummy empty initial message, then send poll using the bot
+        let poll = MockMessagePoll::new();
+        let env = Environment::new(db_pool, poll);
+        env.bot_dispatch().await;
+
+        assert_eq!(env.bot_responses().sent_messages_poll.len(), 1);
+
+        // let message = MockMessageText::new().text("/go");
+        // env.update(message);
+        // env.bot_dispatch().await;
+    }
+
+    async fn on_existing_poll_go_cmd_ignores_telegram_error(db_pool: PgPool) {
+        // let message = MockMessageText::new().text("/go");
+        // let env = Environment::new(db_pool, message);
+        // env.create_poll().await;
+        // env.bot_dispatch().await;
+
+        // let responses = env.bot_responses();
+        // assert_eq!(responses.sent_messages.len(), 1);
+        // let message = responses.sent_messages.last().expect("No sent messages were detected!");
+        // assert_eq!(message.text(), Some("Будь ласка, завершіть поточне голосування."));
+    }
+
+    async fn on_nobody_voted_go_cmd_sends_notice_and_deletes_poll(db_pool: PgPool) {}
+
+    async fn on_voted_go_cmd_sends_the_list_of_voters_and_deletes_poll(db_pool: PgPool) {}
 }
